5133b35bd1325b1540e90543e6113afe1a434d80
mertamal <mertamal@uc.cl>
2022-09-19 22:42:53 -0300

    feat(users): generate users registration controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 # /config/routes.rb
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :games
   root "games#index"
