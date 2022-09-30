16ca0cfe777445af3c28b437d80a2f35916e6d13
meretamal <meretamal@uc.cl>
2022-09-30 09:58:31 -0300

    feature(users): add a users factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
+require 'faker'
+
 FactoryBot.define do
   factory :user do
-    
+    name { Faker::Name.name }
+    password { Faker::Internet.password }
+    email { Faker::Internet.email }
   end
 end
