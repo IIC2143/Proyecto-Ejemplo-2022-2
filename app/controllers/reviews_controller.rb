3edad95f2af4677e6a55b6e6f286c3647a8efbc6
meretamal <meretamal@uc.cl>
2022-10-06 08:21:37 -0300

    refactor: remove unnecessary files generated by scaffold

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,50 +1,42 @@
 class ReviewsController < ApplicationController
-  # GET /reviews/new
   def new
     @game_id = params[:game_id]
     @review = Review.new
   end
 
-  # POST /reviews or /reviews.json
   def create
     review_params[:game_id] = params[:game_id].to_i
     @review = Review.new(review_params)
     @review.user = current_user
 
     if @review.save
-      redirect_to game_path(@review.game.id), notice: "Review was successfully created."
+      redirect_to game_path(@review.game.id)
     else
       render :new
     end
   end
 
-  # PATCH/PUT /reviews/1 or /reviews/1.json
   def update
     respond_to do |format|
       if @review.update(review_params)
-        format.html { redirect_to review_url(@review), notice: "Review was successfully updated." }
-        format.json { render :show, status: :ok, location: @review }
+        redirect_to review_url(@review)
       else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @review.errors, status: :unprocessable_entity }
+        render :edit
       end
     end
   end
 
-  # DELETE /reviews/1 or /reviews/1.json
   def destroy
     @review = Review.find(params[:id])
     @review.destroy
-    redirect_to game_path(@review.game), notice: "Review was successfully destroyed."
+    redirect_to game_path(@review.game)
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_review
       @review = Review.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def review_params
       params.require(:review).permit(:title, :description, :score, :game_id)
     end
